17fa07cfa4dd69dc275de773a5bb199e5cb96aa5
Yutarotaro <9343783873a@g.ecc.u-tokyo.ac.jp>
2023-12-14 23:20:59 +0900

    頂点を1つ追加。マウスで移動できるようにした。

--- src/ProcessImage.js
+++ src/ProcessImage.js
@@ -1,78 +1,52 @@
 import { useState, useRef, useEffect } from 'react';
-import { PrimaryButton, Stack, Text } from '@fluentui/react';
-import drawRectangle from './drawRectangle';
+import { PrimaryButton, Stack, Text, canUseDOM } from '@fluentui/react';
 import { useStateContext } from './StateContext'; // StateContextのインポート
 
 function ProcessImage() {
   const [image, setImage] = useState(null);
   const canvasRef = useRef(null);
   const fileInputRef = useRef(null);
-  const [square, setSquare] = useState({ x: 50, y: 50, size: 100, dragging: false });
 
   const { activeState, setActiveState } = useStateContext();
 
+  const [circlePos, setCirclePos] = useState({ x: 100, y: 100 });
+  const [isDragging, setIsDragging] = useState(false);
+
+
 
   useEffect(() => {
-    if (image) {
-      const canvas = canvasRef.current;
-      const ctx = canvas.getContext('2d');
-      const img = new Image();
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
 
+    if (image) {
+      const img = new Image();
       img.onload = () => {
-        canvas.width = img.width;
-        canvas.height = img.height;
-        ctx.drawImage(img, 0, 0, img.width, img.height);
+        // 画像のアスペクト比に基づいてCanvasの高さを計算
+        const aspectRatio = img.height / img.width;
+        const canvasHeight = canvas.width * aspectRatio;
+  
+        // Canvasの高さを動的に設定
+        canvas.height = canvasHeight;
+  
+        // 画像をリサイズして描画
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
         setActiveState(2);
-        drawSquare(); // 画像がロードされた後に正方形を描画
+        drawCircle(ctx, circlePos.x, circlePos.y); // ここで円を描画
       };
 
       img.src = image;
+    } else {
+      canvas.height = 1000;
+      // drawCircle(ctx, circlePos.x, circlePos.y); // ここで円を描画
     }
-  }, [image]);
+  }, [image, circlePos]); // circlePosを依存関係に追加
 
-  const drawSquare = () => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d');
-    ctx.lineWidth = 10; // ここで線の太さを設定
-    ctx.strokeStyle = 'blue';
-    // 既存の描画をクリアする
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // 画像を再描画
-    if (image) {
-      const img = new Image();
-      img.src = image;
-      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-    }
-    // 正方形を描画
-    ctx.strokeRect(canvas.width / 4, canvas.height / 4, canvas.width / 2, canvas.height / 2);
-    // ctx.strokeRect(square.x, square.y, square.size, square.size);
-    // ハンドルの描画
-    const handles = handlePositions();
-    handles.forEach(handle => {
-      ctx.fillStyle = 'red';
-      ctx.fillRect(handle.x, handle.y, 10, 10);
-    });
-  };
-
-  // 新しいステートを追加：ハンドルのドラッグ状態を管理
-  const [draggingHandle, setDraggingHandle] = useState(null);
-
-  // ハンドルのサイズと位置を計算する関数
-  const handlePositions = () => {
-    const { x, y, size } = square;
-    return [
-      { x: x - 5, y: y - 5 }, // 左上
-      { x: x + size - 5, y: y - 5 }, // 右上
-      { x: x + size - 5, y: y + size - 5 }, // 右下
-      { x: x - 5, y: y + size - 5 } // 左下
-    ];
-  };
-
-  const handleFileSelect = (e) => {
-    const file = e.target.files[0];
-    if (file && file.type.startsWith('image/')) {
-      setImage(URL.createObjectURL(file));
-    }
+  // 丸の描画
+  const drawCircle = (context, x, y) => {
+    context.fillStyle = 'blue';
+    context.beginPath();
+    context.arc(x, y, 10, 0, 2 * Math.PI);
+    context.fill();
   };
 
   const handleDragOver = (e) => {
@@ -90,77 +64,75 @@ function ProcessImage() {
     }
   };
 
+  // ファイル入力変更時のハンドラ
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => setImage(e.target.result);
+      reader.readAsDataURL(file);
+    }
+  };
+
   const handleClick = () => {
     fileInputRef.current.click();
   };
 
   const handleMouseDown = (e) => {
-    const rect = canvasRef.current.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-    // 矩形内をクリックしたか判定
-    if (x > square.x && x < square.x + square.size &&
-      y > square.y && y < square.y + square.size) {
-      setSquare({ ...square, dragging: true });
+    const canvas = canvasRef.current;
+    const rect = canvas.getBoundingClientRect();
+  
+    // 実際の表示サイズと属性で設定されたサイズの比率を計算
+    const scaleX = canvas.width / rect.width;
+    const scaleY = canvas.height / rect.height;
+  
+    // マウスの座標をCanvasのスケールに合わせて調整
+    const x = (e.clientX - rect.left) * scaleX;
+    const y = (e.clientY - rect.top) * scaleY;
+  
+    // 円の範囲内かどうかをチェック（円の中心からの距離を計算）
+    const distance = Math.sqrt(Math.pow(x - circlePos.x, 2) + Math.pow(y - circlePos.y, 2));
+  
+    // 円の半径（ここでは10としている）以内かどうかを判定
+    if (distance < 10) {
+      setIsDragging(true);
     }
-
-    // ハンドルのドラッグ開始を検出
-    const handles = handlePositions();
-    handles.forEach((handle, index) => {
-      if (e.clientX >= handle.x && e.clientX <= handle.x + 10 &&
-        e.clientY >= handle.y && e.clientY <= handle.y + 10) {
-        setDraggingHandle(index);
-      }
-    });
   };
+  
 
   const handleMouseMove = (e) => {
-    if (square.dragging) {
-      const rect = canvasRef.current.getBoundingClientRect();
-      const x = e.clientX - rect.left;
-      const y = e.clientY - rect.top;
-      setSquare({ ...square, x: x - square.size / 2, y: y - square.size / 2 });
-      drawSquare(); // マウス移動時に正方形を再描画
-    }
-
-    // ハンドルがドラッグされている場合の処理
-    if (draggingHandle !== null) {
-      const rect = canvasRef.current.getBoundingClientRect();
-      const x = e.clientX - rect.left;
-      const y = e.clientY - rect.top;
-
-      // ハンドルの位置に応じて正方形のサイズを更新
-      if (draggingHandle === 0) { // 左上のハンドル
-        setSquare({ ...square, x, y, size: square.size + square.x - x });
-      } else if (draggingHandle === 1) { // 右上のハンドル
-        setSquare({ ...square, y, size: x - square.x });
-      } else if (draggingHandle === 2) { // 右下のハンドル
-        setSquare({ ...square, size: x - square.x });
-      } else if (draggingHandle === 3) { // 左下のハンドル
-        setSquare({ ...square, x, size: square.size + square.y - y });
-      }
-      drawSquare();
+    if (isDragging) {
+      const canvas = canvasRef.current;
+      const rect = canvas.getBoundingClientRect();
+  
+      // 実際の表示サイズと属性で設定されたサイズの比率を計算
+      const scaleX = canvas.width / rect.width;
+      const scaleY = canvas.height / rect.height;
+  
+      // マウスの座標をCanvasのスケールに合わせて調整
+      const x = (e.clientX - rect.left) * scaleX;
+      const y = (e.clientY - rect.top) * scaleY;
+  
+      setCirclePos({ x, y });
+      console.log("Cicle pos is set to (x, y)", x, y);
     }
   };
 
   const handleMouseUp = () => {
-    setSquare({ ...square, dragging: false });
-    setDraggingHandle(null);
+    setIsDragging(false);
   };
-
-
-
 
   return (
     <div>
-      <Stack tokens={{ childrenGap: 20 }} padding={20}>
+      <Stack tokens={{ childrenGap: 20 }} >
         <input
           type="file"
           accept="image/*"
           style={{ display: 'none' }}
           ref={fileInputRef}
-          onChange={handleFileSelect}
+          onChange={handleFileChange}
         />
+        {activeState == 1 && (
         <Stack
           horizontalAlign="center"
           verticalAlign="center"
@@ -181,12 +153,13 @@ function ProcessImage() {
         >
           <Text>ドラッグアンドドロップ、またはタップして画像をアップロード</Text>
         </Stack>
+        )}
         <canvas
+          width={2000} 
           ref={canvasRef}
           onMouseDown={handleMouseDown}
           onMouseMove={handleMouseMove}
           onMouseUp={handleMouseUp}
-          onMouseOut={handleMouseUp}
           style={{ maxWidth: '100%' }} />
       </Stack>
     </div>
